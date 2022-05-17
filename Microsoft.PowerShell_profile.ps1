edf2a0594017f1551e9f06a6905568552b5799b5
daplho <nskirov@gmail.com>
2022-05-17 15:46:17 -0700

    improved powershell profile * added posh-git * added psreadline option for saving but not executing a command

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,8 @@
+# https://github.com/JanDeDobbeleer/oh-my-posh
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerline.omp.json" | Invoke-Expression
+
+# https://github.com/dahlbyk/posh-git
+import-module posh-git
 
 # https://github.com/PowerShell/PSReadLine
 Import-Module PSReadLine
@@ -7,6 +11,23 @@ Import-Module PSReadLine
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Sometimes you enter a command but realize you forgot to do something else first.
+# This binding will let you save that command in the history so you can recall it,
+# but it doesn't actually execute. It also clears the line with RevertLine so the
+# undo stack is reset - though redo will still reconstruct the command line.
+
+set-psreadlinekeyhandler -key alt+w `
+                         -briefdescription SaveInHistory `
+			 -longdescription "Save current line in history but do not execute" `
+			 -scriptblock {
+  param($key, $arg)
+  $line = $null
+  $cursor = $null
+  [microsoft.powershell.psconsolereadline]::getbufferstate([ref]$line, [ref]$cursor)
+  [microsoft.powershell.psconsolereadline]::addtohistory($line)
+  [microsoft.powershell.psconsolereadline]::revertline()
+}
 
 # Insert text from the clipboard as a string
 Set-PSReadLineKeyHandler -Key Ctrl+V `
