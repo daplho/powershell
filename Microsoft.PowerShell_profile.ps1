a2238cae6779e6f54633c141e2d169fc316cb38c
daplho <nskirov@gmail.com>
2022-08-10 09:25:19 -0700

    Added `vim` to the path.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -66,6 +66,7 @@ Function GitStatus {git status}
 Set-Alias -Name gs -Value GitStatus
 
 # Path
+$env:PATH += ";C:\Program Files\Vim\vim90"
 $env:PATH += ";C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin"
 $env:PATH += ";C:\Users\nikokirov\.dotnet\Microsoft.Net.Compilers.4.2.0\tools"
 $env:PATH += ";C:\Users\nikokirov\.dotnet\nuget"
